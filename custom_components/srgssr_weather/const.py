e6129cbc619042bcc52a16bf63cd215ce88ba76e
Stefan Agner <stefan@agner.ch>
2021-07-06 23:45:40 +0200

    Extend config flow to get required geolocation from API

--- custom_components/srgssr_weather/const.py
+++ custom_components/srgssr_weather/const.py
@@ -4,9 +4,11 @@ HOME_LOCATION_NAME = "Home"
 
 CONF_CONSUMER_KEY = "consumer_key"
 CONF_CONSUMER_SECRET = "consumer_secret"
+CONF_GEOLOCATION_ID = "geolocation_id"
 
 ATTR_EXPIRES_AT = "expires_at"
 ATTR_API_KEY = "api_key"
 
-ERROR_LOCATION_EXISTS = "location_exists"
 ERROR_INVALID_CREDENTIALS = "invalid_credentials"
+ERROR_NO_GEOLOCATION_FOUND = "no_geolocation_found"
+ERROR_GEOLOCATION_EXISTS = "geolocation_exists"
