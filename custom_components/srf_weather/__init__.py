d5968ca1f48d8a8b3ee18e2572ca5ff20d0d8993
Simon Berger <simon@siku2.io>
2023-09-07 16:42:22 +0000

    add the config entry migration

--- custom_components/srf_weather/__init__.py
+++ custom_components/srf_weather/__init__.py
@@ -1,5 +1,9 @@
+import logging
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+
+_LOGGER = logging.getLogger(__name__)
 
 PLATFORMS = ["weather"]
 
@@ -12,3 +16,15 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     return ok
+
+
+async def async_migrate_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+    _LOGGER.debug("migrating from version %s", config_entry.version)
+
+    if config_entry.version == 1:
+        # no changes required for this version
+        config_entry.version = 2
+
+    _LOGGER.info("migration to version %s successful", config_entry.version)
+
+    return True
